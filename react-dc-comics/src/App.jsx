4f5e6947e581e31c4a0cc728aee34fa1d14b9c32
AndreaMeroi <andreameroidublin@gmail.com>
2025-09-22 20:04:05 +0200

    add font

--- react-dc-comics/src/App.jsx
+++ react-dc-comics/src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import logo Header 
-import headerLogo from './assets/img/'
+import headerLogo from './assets/img/dc-logo.png'
 
 //Importa Header
 
@@ -21,8 +21,28 @@ function App() {
     <>
 
       <header>
-        <img src="headerLogo" alt="" />
+
+        <div className='Header-centrato'>
+
+          <img src={headerLogo} alt="logo header" />
+
+          <nav>
+            <a href=''>CHARACTERS</a>
+            <a href=''>COMICS</a>
+            <a href=''>MOVIES</a>
+            <a href=''>TV</a>
+            <a href=''>GAMES</a>
+            <a href=''>COLLECTIBLES</a>
+            <a href=''>VIDEOS</a>
+            <a href=''>FANS</a>
+            <a href=''>NEWS</a>
+            <a href=''>SHOP</a>
+          </nav>
+
+        </div>
+
       </header>
+
       <main></main>
       <footer></footer>
 
--- react-dc-comics/src/main.jsx
+++ react-dc-comics/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Import Font step1
+import '@fontsource-variable/open-sans';
 import './index.css'
 import App from './App.jsx'
 
