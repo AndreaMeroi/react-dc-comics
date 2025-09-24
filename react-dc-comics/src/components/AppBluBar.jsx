51c1c876af9909c92388d279120c2b6ef4c10c98
AndreaMeroi <andreameroidublin@gmail.com>
2025-09-24 18:07:15 +0200

    add last changes

--- react-dc-comics/src/components/AppBluBar.jsx
+++ react-dc-comics/src/components/AppBluBar.jsx
@@ -1,4 +1,8 @@
 import BuyDgImg from '../assets/img/buy-comics-digital-comics.png'
+import MerchImg from '../assets/img/buy-comics-merchandise.png'
+import SubImg from '../assets/img/buy-comics-subscriptions.png'
+import locatorImg from '../assets/img/buy-comics-shop-locator.png'
+import PowerVisaImg from '../assets/img/buy-dc-power-visa.svg'
 
 export default function AppBluBar() {
     return (
@@ -9,13 +13,22 @@ export default function AppBluBar() {
                 <a href=""> digital comics</a>
             </div>
             <div className="BluBarItem">
-                <img src={BuyDgImg} alt="Buy DG Img" />
-                <a href=""> digital comics</a>
+                <img src={MerchImg} alt="Buy DG Img" />
+                <a href=""> dc merchandise</a>
             </div>
             <div className="BluBarItem">
-                <img src={BuyDgImg} alt="Buy DG Img" />
-                <a href=""> digital comics</a>
+                <img src={SubImg} alt="Buy DG Img" />
+                <a href=""> subscription</a>
             </div>
+            <div className="BluBarItem">
+                <img src={locatorImg} alt="Buy DG Img" />
+                <a href=""> comic shop locator</a>
+            </div>
+            <div className="BluBarItem">
+                <img src={locatorImg} alt="Buy DG Img" />
+                <a href=""> comic shop locator</a>
+            </div>
+
         </nav>
 
     )
