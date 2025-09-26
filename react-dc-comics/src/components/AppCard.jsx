0411c90ab0eaa6dba60992b7fa6dcca4396341f5
AndreaMeroi <andreameroidublin@gmail.com>
2025-09-26 15:55:10 +0200

    add cards2

--- react-dc-comics/src/components/AppCard.jsx
+++ react-dc-comics/src/components/AppCard.jsx
@@ -1,6 +1,8 @@
+import style from './AppCard.module.css'
+
 export default function AppCard(promps) {
     return (
-        <div className='card'>
+        <div className={style.card}>
             <img src={promps.img} alt="" />
             <h3> {promps.title} </h3>
         </div>
